d3c39a7211dc7c862dcd128342ebc4b94d0fc87c
su12 <ts04109@naver.com>
2024-02-18 23:54:41 +0900

    feat : 자체 회원가입 기능

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import Join
+from rest_framework_simplejwt.views import TokenRefreshView
+
+app_name = 'user'
 
 urlpatterns = [
-    
+    path('join/', Join.as_view(), name='join'),
 ]
+
--- user/views.py
+++ user/views.py
@@ -1,4 +1,46 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+import requests
 
 # Create your views here.
 
+
+
+class Join(APIView):
+    def post(self, request):
+        
+        user_data = {
+            "email" : request.data.get('email'),
+            "password" : request.data.get('password')
+        }
+
+        serializer = UserSerializer(data=user_data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            users_data = {
+                "user": user.id,
+            }
+
+            token = RefreshToken.for_user(user)
+            access_token = str(token.access_token)
+            refresh_token = str(token)
+
+            message = {
+                "message": "회원가입 성공",
+                "user": users_data,
+                "token": {
+                    "access": access_token,
+                    "refresh": refresh_token,
+                }
+            }
+
+            return Response(message, status=status.HTTP_200_OK)
+    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
