a7ff8a2b16a3036c6e38199c86045df004171602
su12 <ts04109@naver.com>
2024-02-19 00:54:04 +0900

    feat : 자체 로그인 기능

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Join
+from .views import Join, Login
 from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = 'user'
 
 urlpatterns = [
     path('join/', Join.as_view(), name='join'),
+    path('login/', Login.as_view(), name='login'),
 ]
 
--- user/views.py
+++ user/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User
 from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .serializers import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 import requests
@@ -44,3 +46,34 @@ class Join(APIView):
             return Response(message, status=status.HTTP_200_OK)
     
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class Login(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = authenticate(
+            email = email,
+            password = password
+        )
+
+        if user is not None and user.is_active:
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user" : user.id,
+                    "message" : "Login success",
+                    "token" : {
+                        "access" : access_token,
+                        "refresh" : refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            return res
+        else : 
+            return Response({"error" : "이메일 또는 비밀번로가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
