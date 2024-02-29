4ba228bdee3cce9ed931fb6a82af98c5d15100d9
kimyeoju <yeojoo0031@gmail.com>
2024-02-29 15:53:52 +0900

    feat : s3 연동 설정 및 uploads.py 코드 추가

---
+++ post/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Post, Comment, PostImage
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ['image']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    images = PostImageSerializer(many=True, read_only=True)
+    
+    def get_images(self, obj):
+        image = obj.image_set.all()
+        return PostImageSerializer(instance=image, many=True, context=self.context).data
+    
+    class Meta:
+        model = Post
+        exclude = ['writer']
+    
+    def create(self, validated_data):
+        images_data = self.context['request'].FILES.getlist('images')
+        instance = Post.objects.create(**validated_data)
+        
+        for image_data in images_data:
+            PostImage.objects.create(post=instance, image=image_data)
+            
+        return instance
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('index/', views.Index.as_view(), name='index'),
+    path('postwrite/', views.PostWrite.as_view(), name='postwrite'),
+    path('upload/', views.PostImage.as_view(), name='upload'),
 ]
