84f5a10335e6067fbc619154962e2c018f204fd3
kimyeoju <yeojoo0031@gmail.com>
2024-03-02 19:49:31 +0900

    feat : s3 연동 설정 및 다중 이미지 글 생성 기능 추가

--- post/views.py
+++ post/views.py
@@ -15,33 +15,11 @@ import json
 
 User = get_user_model()
 
-
-class PostIndex(APIView):
+class Index(APIView):
     def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        post = Post.objects.all()
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+        
 
-class PostCreate(APIView):
-    def post(self, request):
-        user = request.user
-        post_data = {
-            'title': request.data['title'],
-            'content': request.data['content'],
-            'writer': user
-        }
-        images = request.FILES.getlist('image')
-        print(images)
-        post = Post.objects.create(**post_data)
-        print(post)
-        for image in images:
-            img_uploader = S3ImgUploader(image)
-            folder = 'post_image'
-            uploaded_url = img_uploader.upload(folder)
-
-            PostImage.objects.create(post=post,image=uploaded_url)
-            
-        data = {
-            "message" : "글 생성 완료 "
-        }
-        return Response(data, status=status.HTTP_201_CREATED)
+    
