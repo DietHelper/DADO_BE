9f971c0653b3fbf731d22efaaa07dab6d29760dc
su12 <ts04109@naver.com>
2024-03-06 00:07:05 +0900

    chore : 충돌 파일 변경

--- post/urls.py
+++ post/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('index/', views.Index.as_view(), name='index'),
-    path('postwrite/', views.PostWrite.as_view(), name='postwrite'),
+    path('postwrite/', views.PostCreate.as_view(), name='postwrite'),
     path('upload/', views.PostImage.as_view(), name='upload'),
 ]
