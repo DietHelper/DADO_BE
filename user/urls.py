2e66f67196ff2939b246f5ca7c1abf606e0763b1
su12 <ts04109@naver.com>
2024-03-07 00:54:59 +0900

    feat :  프로필 조회 기능 구현

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import Join, Login, Logout
+from . import views
 from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = 'user'
 
 urlpatterns = [
-    path('join/', Join.as_view(), name='join'),
-    path('login/', Login.as_view(), name='login'),
-    path('logout/', Logout.as_view(), name='logout'),
+    path('join/', views.Join.as_view(), name='join'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    # path('profile/<int:user_id>/', views.ProfileView.as_view(), name='profile-detail'),
 ]
 
