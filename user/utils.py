93fca725e3b821be9cf921406d0cafe6f07c08f1
su12 <ts04109@naver.com>
2024-03-15 21:09:27 +0900

    feat : (회원가입) 이메일 인증 기능 구현

---
+++ user/utils.py
@@ -0,0 +1,14 @@
+import random
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.conf import settings
+
+def generate_otp():
+    otp = str(random.randint(100000, 999999))
+    return otp
+
+def send_otp_via_email(email, otp):
+    subject = '[DADO] 환영합니다! 이메일을 인증해주세요.'
+    context = {'otp':otp, 'email':settings.EMAIL_HOST_USER}
+    message = render_to_string('user/email_template.html', context)
+    send_mail(subject,'',settings.EMAIL_HOST_USER, [email], html_message=message)
