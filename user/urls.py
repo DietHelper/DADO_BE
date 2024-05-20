c9b2d19fa582f0c9f553c1164e9a0687ba98e1aa
su12 <ts04109@naver.com>
2024-05-20 23:51:22 +0900

    feat : 팔로우 기능 구현

--- user/urls.py
+++ user/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('otp/', views.GenerateOtp.as_view(), name='otp'),
     # path('profile/<int:user_id>/', views.ProfileView.as_view(), name='profile-detail'),
     path('profile-edit/', views.ProfileEdit.as_view(), name='profile-edit'),
+    path('<int:user_id>/follow/', views.Follow.as_view(), name='follow'),
 ]
 
