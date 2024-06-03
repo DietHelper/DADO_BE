93ac687703784bca6c5434d82de6ce56bb62341e
su12 <ts04109@naver.com>
2024-06-03 22:05:07 +0900

    feat : 게시글 목록, 작성 로그인 인증 추가

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'post'
+
 urlpatterns = [
     path('index/', views.PostIndex.as_view(), name='index'),
     path('postcreate/', views.PostCreate.as_view(), name='post_create'),
