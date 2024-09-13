6a9b0f231a14f375f4a19412fbde54cd9247aee0
su12 <ts04109@naver.com>
2024-09-13 15:52:13 +0900

    feat : crew join 기능 추가

--- crew/urls.py
+++ crew/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import CrewJoin
 
 urlpatterns = [
-    
+    path("join/", CrewJoin.as_view(), name='join'),
 ]
--- crew/views.py
+++ crew/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .models import Crew, Tag
+from .serializers import CrewSerializer, TagSeirializer
 
 # Create your views here.
+
+class CrewJoin(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        crew_id = request.data.get('crew_id')
+        crew = get_object_or_404(Crew, id=crew_id)
+
+        if crew.participants.count() < crew.max_participants:
+
+            if request.user not in crew.participants.all():
+
+                if crew.participants.count() == crew.max_participants-1:
+                    crew.status = '진행중'
+                    crew.save()
+                
+                crew.participants.add(request.user)
+                return Response({"message":"크루 참가 완료되었습니다."}, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message":"이미 참가 중인 크루입니다."}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"messsage":"참가 인원이 마감되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
+    
